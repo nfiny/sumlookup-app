d6bc86f43d0d882047d26f6d9af4a298e10e20dd
jaenam <jae@nfiny.com>
2022-02-10 15:36:48 +1100

    website v1 complete

--- js/main.js
+++ js/main.js
@@ -190,7 +190,7 @@
   $(".clients-carousel").owlCarousel({
     autoplay: true,
     dots: true,
-    loop: true,
+    loop: false,
     responsive: { 0: { items: 2 }, 768: { items: 4 }, 900: { items: 6 }
     }
   });
